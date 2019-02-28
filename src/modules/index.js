36a6113de7f18e5c250b304013c6cb6ab6abf9e3
Alan Boglioli <alan.boglioli@gmail.com>
2019-02-27 22:37:04 -0300

    replace glob by fileLoader from merge-graphql-schemas

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,16 +1,9 @@
-const { mergeTypes, mergeResolvers } = require('merge-graphql-schemas');
+const { mergeTypes, mergeResolvers, fileLoader } = require('merge-graphql-schemas');
 const path = require('path');
-const fs = require('fs');
-const glob = require('glob');
 const { makeExecutableSchema } = require('graphql-tools');
 
-const types = glob
-  .sync(path.join(__dirname, './**/*.graphql'))
-  .map(file => fs.readFileSync(file, { encoding: 'utf-8' }));
-
-const resolvers = glob
-  .sync(path.join(__dirname, './**/*resolvers.*'))
-  .map(file => require(file));
+const types = fileLoader(path.join(__dirname, './**/*.graphql'));
+const resolvers = fileLoader(path.join(__dirname, './**/*resolvers.*'));
 
 module.exports = makeExecutableSchema({
   typeDefs: mergeTypes(types),
